2bf822a55cb0c39958dcbc9babb5909db43d653b
rifayetazamtalukder <rifayetewucse@gmail.com>
2019-12-25 14:00:08 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TicTacToeConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set Background and Foreground Color
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Clear();
+
+            // Code For Tic Tac Toe
+
+            Console.WriteLine("Tic Tac Toe");
+
+
+
+            // End of Code
+            Console.ReadKey();
+        }
+    }
+}
